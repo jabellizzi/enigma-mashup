bdf099105bc95cbf948641a23088721071ec1355
John Bellizzi <jabellizzi406@gmail.com>
2018-03-23 13:28:33 -0700

    Activity 2

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ var session = enigma.create(config);
 session.open().then(function(qlik) {
 
   qlik.openDoc('WBY Sales.qvf').then(function(app) {
-    var myDef = {
+    var pieDef = {
       qInfo: {
-        qType: 'myBarChart'
+        qType: 'myPieChart'
       },
       qHyperCubeDef: {
         qDimensions: [
           {
             qDef: {
-              qFieldDefs: ['Country']
+              qFieldDefs: ['CategoryName']
             }
           }
         ],
@@ -24,34 +24,59 @@ session.open().then(function(qlik) {
           {
             qDef: {
               qDef: "=Sum(OrderLineAmount)"
-            },
-            qSortBy: {
-              qSortByNumeric: -1
             }
           }
         ],
-        qInterColumnSortOrder: [1, 0],
         qInitialDataFetch: [
           {
             qTop: 0,
             qLeft: 0,
             qWidth: 2,
-            qHeight: 100
+            qHeight: 50
           }
         ]
       }
     };
 
     // Create Generic Object with HyperCube
-    app.createSessionObject(myDef).then(function(model) {
-
+    app.createSessionObject(pieDef).then(function(model) {
+      // Render PIE every time the model changes
+      model.addListener('changed', function() {
+        renderPie(model, 'PIE');
+      });
+      // Render PIE when the object initizalizes
+      renderPie(model, 'PIE');
     })
   })
 })
 
 // PIE
 function renderPie(model, elementId) {
+  model.getLayout().then(function(layout) {
+    var qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
+    // Reformatting qMatrix data into amChart format
+    var amData = qMatrix.map(function(qMatrixRow) {
+      return {
+        dim: qMatrixRow[0].qText,
+        exp: qMatrixRow[1].qNum,
+        elemNumber: qMatrixRow[0].qElemNumber
+      }
+    });
 
+    // Render Pie Chart
+    AmCharts.makeChart(elementId, {
+      type: 'pie',
+      valueAxes: [{
+        axisAlpha: 1
+      }],
+      radius: '42%',
+      innerRadius: '60%',
+      dataProvider: amData,
+      titleField: 'dim',
+      valueField: 'exp',
+      labelText: '[[dim]]'
+    })
+  })
 }
 
 
